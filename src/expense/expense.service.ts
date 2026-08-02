4faf6c49b8c3c95a30a891b85a725d7b098fc064
mbhandari929 <dadhirambaral664@gmail.com>
2026-08-02 18:43:11 +0900

    Improve expense CRUD validation

--- src/expense/expense.service.ts
+++ src/expense/expense.service.ts
@@ -1,15 +1,19 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Expense } from './entities/expense.entity';
-import { CreateExpenseDto } from './dto/create-expense.dto';
-import { UpdateExpenseDto } from './dto/update-expense.dto';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Expense } from "./entities/expense.entity";
+import { CreateExpenseDto } from "./dto/create-expense.dto";
+import { UpdateExpenseDto } from "./dto/update-expense.dto";
 
 @Injectable()
 export class ExpenseService {
   constructor(
     @InjectRepository(Expense)
-    private expenseRepository: Repository<Expense>,
+    private readonly expenseRepository: Repository<Expense>,
   ) {}
 
   create(createExpenseDto: CreateExpenseDto) {
@@ -21,16 +25,44 @@ export class ExpenseService {
     return this.expenseRepository.find();
   }
 
-  findOne(id: number) {
-    return this.expenseRepository.findOneBy({ id });
+  async findOne(id: number) {
+    if (!Number.isInteger(id)) {
+      throw new BadRequestException("Invalid expense ID");
+    }
+
+    const expense = await this.expenseRepository.findOneBy({ id });
+
+    if (!expense) {
+      throw new NotFoundException(`Expense with ID ${id} not found`);
+    }
+
+    return expense;
   }
 
   async update(id: number, updateExpenseDto: UpdateExpenseDto) {
-    await this.expenseRepository.update(id, updateExpenseDto);
-    return this.expenseRepository.findOneBy({ id });
+    if (!Number.isInteger(id)) {
+      throw new BadRequestException("Invalid expense ID");
+    }
+
+    const expense = await this.expenseRepository.preload({
+      id,
+      ...updateExpenseDto,
+    });
+
+    if (!expense) {
+      throw new NotFoundException(`Expense with ID ${id} not found`);
+    }
+
+    return this.expenseRepository.save(expense);
   }
 
-  remove(id: number) {
-    return this.expenseRepository.delete(id);
+  async remove(id: number) {
+    if (!Number.isInteger(id)) {
+      throw new BadRequestException("Invalid expense ID");
+    }
+
+    const expense = await this.findOne(id);
+
+    return this.expenseRepository.remove(expense);
   }
 }
