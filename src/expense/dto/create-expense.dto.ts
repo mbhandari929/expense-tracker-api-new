153ecb043624a0b0480bd0fa9e079acc89d64ac9
mbhandari929 <dadhirambaral664@gmail.com>
2026-08-03 15:14:43 +0900

    Add expense validation and global validation pipe

--- src/expense/dto/create-expense.dto.ts
+++ src/expense/dto/create-expense.dto.ts
@@ -1,1 +1,20 @@
-export class CreateExpenseDto {}
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+} from "class-validator";
+
+export class CreateExpenseDto {
+  @IsString()
+  @IsNotEmpty()
+  text!: string;
+
+  @IsNumber()
+  @IsPositive()
+  amount!: number;
+
+  @IsDateString()
+  date!: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-
+import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+app.useGlobalPipes(
+  new ValidationPipe({
+    whitelist: true,
+    transform: true,
+  }),
+);
   app.enableCors({
     origin: "http://localhost:5173",
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
